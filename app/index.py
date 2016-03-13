b574003bd2a96c5716bae91d769b8ddbd966a42f
Marc Preston <phyrenight@gmail.com>
2016-03-13 01:41:50 -0800

    pep8 updates

--- app/index.py
+++ app/index.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template, request, redirect, jsonify, url_for, flash
+from flask import Flask, render_template, request, redirect
+from flask import jsonify, url_for, flash
 from teamsdb import User, Teams, Players, Base
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
@@ -7,18 +8,39 @@ from flask from session as session as login_session
 
 app = Flask(__name__)
 
+
 @app.route('welcome')
 def welcome():
     return "welcome"
 
 
 @app.route('/login')
-def login()
+def login():
     return "login screen"
 
 
 @app.route('/teams')
 def displayTeams():
-    return "and here are the teams."
+    teams = session.query(Teams).all()
+    return render_template('displayTeams.html', teams=teams)
 
-@app.route(/teams/)
+
+@app.route('/teams/<team>')
+def displayTeamInfo():
+    return "or maybe not"
+
+
+@app.route('/teams/<team>/players/')
+def teamPlayers():
+    return "and here are your players"
+
+
+@app.route('/teams/<team>/players/<name>')
+def playerInfo():
+    return "enough info for you"
+
+
+if __name__ == "__main__":
+    app.secret_key = "not yet"
+    app.run(host='0.0.0.0', port=5000)
+    app.debug = True
